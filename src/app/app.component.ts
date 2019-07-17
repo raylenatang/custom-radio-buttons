9eb11e19772eb557f91ad7d5b2252a720b2efcf0
Ruining Tang <ruinint@uci.edu>
2019-07-18 05:55:56 +0800

    set up radio buttons group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'materialDesign';
+  choice: string;
+  numbers: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 }
