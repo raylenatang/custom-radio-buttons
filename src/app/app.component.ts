3a9182ed16bd371417d31560c0cce981c5a17b8a
Zhaoxian Hu <huzhx@uci.edu>
2019-07-20 17:43:55 -0700

    add reactive form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  choice: string;
-  numbers: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+export class AppComponent implements OnInit {
+  testForm: FormGroup;
+  numbers: number[];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    this.createForm();
+  }
+
+  createForm() {
+    this.testForm = this.formBuilder.group({
+      degree: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log('click submit button');
+    console.log(this.testForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AppComponent } from "./app.component";
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
